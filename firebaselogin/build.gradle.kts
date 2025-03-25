b8986b84d68f6ccb5e50ea24c8cb40e58c9a9560
Peter Wonham <peterwonham@googlemail.com>
2025-03-25 17:41:23 +0000

    First Commit

--- firebaselogin/build.gradle.kts
+++ firebaselogin/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("maven-publish")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
-    id("kotlin-kapt") // Add kapt for annotation processing
+    id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
 }
 
@@ -63,15 +63,13 @@ dependencies {
 
     implementation("com.google.firebase:firebase-firestore:25.1.2")
 
-//hilt
+    //hilt
     implementation("com.google.dagger:hilt-android:2.52")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     kapt("com.google.dagger:hilt-android-compiler:2.52")
     //Icons
     implementation ("androidx.compose.material:material-icons-extended:1.7.8")
-
-
 
 
     // For Compose testing
@@ -86,53 +84,12 @@ dependencies {
 afterEvaluate {
     publishing {
         publications {
-            release(MavenPublication) {
-                from components.release
-
-                        groupId = 'com.github.T1H2H0'
-                artifactId = 'firebaselogin'
-                version = '1.0'
+            create<MavenPublication>("mavenJava") {
+                from(components["release"])
+                groupId = "com.github.T1H2H0"
+                artifactId = "firebaselogin"
+                version = "1.0.2"
             }
         }
     }
 }
-//publishing {
-//    publications {
-//        create<MavenPublication>("mavenJava") {
-//            from(components["java"])
-//
-//            groupId = "com.uwonham"
-//            artifactId = "firebaselogin"
-//            version = "0.1.0"
-//
-////            // Optionally, if you have sources and Javadoc to publish
-////            artifact(tasks["javadocJar"])
-////            artifact(tasks["sourcesJar"])
-//        }
-//    }
-//}
-//
-//    repositories {
-//        maven {
-//            name = "MavenRepo"
-//            url = uri("https://your.repo.url/repository/maven-releases/")
-//
-//            credentials {
-//                username = System.getenv("MAVEN_USERNAME")
-//                password = System.getenv("MAVEN_PASSWORD")
-//            }
-//        }
-//    }
-//}
-//    publishing {
-//        publications {
-//            create<MavenPublication>("release") {
-//                from(components["release"])
-//
-//            groupId = "com.github.t1h2h0"
-//                artifactId = "firebaselogin"
-//                version = "1.0"
-//            }
-//
-//        }
-//    }
