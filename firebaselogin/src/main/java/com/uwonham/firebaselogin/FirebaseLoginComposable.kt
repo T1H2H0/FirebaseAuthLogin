16d948e3611f52e045be10bd330c8c157e02071e
Peter Wonham <peterwonham@googlemail.com>
2026-07-06 13:11:16 +0100

    Added Custom fields

--- firebaselogin/src/main/java/com/uwonham/firebaselogin/FirebaseLoginComposable.kt
+++ firebaselogin/src/main/java/com/uwonham/firebaselogin/FirebaseLoginComposable.kt
@@ -124,7 +124,6 @@ fun FirebaseSignInDialog(
     allowedEmailDomain: String = "",
     onDismiss: () -> Unit,
     onSignInSuccess: (user: com.google.firebase.auth.FirebaseUser) -> Unit,
-    requiredCustomFields: List<String> = emptyList(),
     customContent: @Composable (customData: SnapshotStateMap<String, Any?>) -> Unit = {}
 ) {
     val viewModel: FirebaseLoginViewModel = hiltViewModel()
@@ -382,8 +381,6 @@ fun FirebaseSignInDialog(
                                         showCreateDialog.value = false
                                         viewModel.checkAccountExists(state.email)
                                     },
-                                    requiredCustomFields =  requiredCustomFields,
-
                                     customContent = customContent
                                 )
                             }
